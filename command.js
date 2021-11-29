918a46fed7fb8ab50e9259013c51095d2561ad33
HidenaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2021-11-29 22:29:06 +0900

    ひな形の作成

--- command.js
+++ command.js
@@ -1,7 +1,35 @@
 const Discord = require('discord.js');
 const { readFileSync } = require("fs");
+const config = require("config.json");
 
 const client = new Discord.Client();
 const TOKEN = readFileSync('./botToken.txt', 'utf8');
+const prefix = config.prefix;
+
+client.on('ready', () => {
+    console.log('Run command.js');
+});
+
+client.on('message', async message => {
+    if (!message.guild.id == "914595171881078784") return;
+    if (!message.content.startsWith(prefix)) return;
+    const [cmd, ...args] = message.content.slice(prefix.length).split(" ");
+    if (cmd == 'hello') {
+        var roleId = "914596371351359548";
+        var welcomeCh = message.guild.channels.cache.get("914597072773206046");
+        var fromArray = ["空から降ってきました。", "迷い込んだようだ。", "拾われたようだ。", "参加しました。", "転がってきました。", "漂着しました", "観光に来ました。"];
+        var from = fromArray[Math.floor(Math.random() * fromArray.length)];
+        message.member.roles.add(`${roleId}`);
+        message.reply({
+            embed: {
+                description: `<@${roleId}>を付与しました👍`,
+                color: "RANDOM"
+            }
+        });
+        welcomeCh.send(`@here <@${message.author.id}>さんが${from}`);
+    } else {
+        message.reply("すみません。\nそのコマンドは存在しないようです😭");
+    }
+})
 
 client.login(TOKEN);
--- command/emoji.js
+++ command/emoji.js
@@ -4,4 +4,8 @@ const { readFileSync } = require("fs");
 const client = new Discord.Client();
 const TOKEN = readFileSync('../botToken.txt', 'utf8');
 
+client.on('ready', () => {
+    console.log('Run command/emoji.js');
+});
+//if (!message.guild.id == "914595171881078784") return;
 client.login(TOKEN);
