7e6abf55cf3c2d3f2c0c86d8220ad401d7776445
HidenaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2021-11-29 22:30:06 +0900

    ステータスメッセージの追加

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const Discord = require('discord.js');
 const { readFileSync } = require("fs");
 
-require("./command.js")
-require("./command/emoji.js")
+require("./command.js");
+require("./command/emoji.js");
 
 const client = new Discord.Client();
 const TOKEN = readFileSync('./botToken.txt', 'utf8');
+
+
+client.on('ready', () => {
+    console.log('Run main.js');
+    client.user.setActivity('Twitter', { type: 'WATCHING' });
+});
 
 client.login(TOKEN);
