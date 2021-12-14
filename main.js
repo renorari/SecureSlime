abc02b7ccfe5fa621e38b428079f3b4794ba84cc
HidenaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2021-12-14 15:54:15 +0900

    ロギング機能の追加

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+const { channel } = require('diagnostics_channel');
 const Discord = require('discord.js');
-const { readFileSync } = require("fs");
+const { readFileSync, appendFileSync, writeFileSync, mkdirSync } = require("fs");
 
 require("./command.js");
 require("./command/emoji.js");
@@ -11,6 +12,23 @@ const TOKEN = readFileSync('./botToken.txt', 'utf8');
 client.on('ready', () => {
     console.log('Run main.js');
     client.user.setActivity('Twitter', { type: 'WATCHING' });
+});
+client.on("message", async (message) => {
+    if (!message.guild.id == "914595171881078784") return;
+    var logFile;
+    var logData = message.author.tag + "< " + message.content + "\n---\n実行者ID: " + message.author.id + "\nMsgID: " + message.id + "\n";
+    var logPath = `chatLog/${message.guild.id}/${message.channel.id}/chat.log`;
+    try {
+        logFile = readFileSync(logPath);
+    } catch (error) {
+        var loggingData = "チャンネルネーム: " + message.channel.name + "\n";
+        mkdirSync(`chatLog/${message.guild.id}/${message.channel.id}/`, { recursive: true });
+        writeFileSync(logPath, loggingData);
+        console.log(logData);
+        appendFileSync(logPath, logData + "\n");
+    }
+    console.log(logData);
+    appendFileSync(logPath, logData + "\n");
 });
 
 client.login(TOKEN);
