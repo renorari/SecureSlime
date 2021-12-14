48f88013849a0643350f17d3dc08931ae7609251
TarochanChannel <gakutyn.2020@gmail.com>
2021-12-14 23:10:47 +0900

    飽きたから途中までｗ

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
-const { channel } = require('diagnostics_channel');
-const Discord = require('discord.js');
+const { Client, Intents } = require("discord.js");
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const { readFileSync, appendFileSync, writeFileSync, mkdirSync } = require("fs");
+const client = new Client({ "intents": new Intents(32767) });
+const TOKEN = readFileSync("./botToken.txt", "utf8");
 
-require("./command.js");
-require("./command/emoji.js");
+const commands = [{
+    name: 'ping',
+    description: 'Replies with Pong!'
+}];
 
-const client = new Discord.Client();
-const TOKEN = readFileSync('./botToken.txt', 'utf8');
+const rest = new REST({ version: '9' }).setToken(TOKEN);
 
+require("./command.js")(client);
+require("./command/emoji.js")(client);
 
-client.on('ready', () => {
-    console.log('Run main.js');
-    client.user.setActivity('Twitter', { type: 'WATCHING' });
+client.on("ready", async () => {
+    console.log("Run main.js");
+    client.user.setActivity("Twitter", { type: "WATCHING" });
+
+    client.guilds.cache.forEach(async (guild) => {
+        try {
+            await rest.put(
+                Routes.applicationGuildCommands(client.user.id, guild.id),
+                { body: commands },
+            );
+        } catch (error) {
+            console.log(error);
+        }
+    });
 });
-client.on("message", async (message) => {
-    if (!message.guild.id == "914595171881078784") return;
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }else{
+
+    };
+});
+
+client.on("messageCreate", async (message) => {
+    if (message.guild.id != "914595171881078784") return;
     var logFile;
     var logData = message.author.tag + "< " + message.content + "\n---\n実行者ID: " + message.author.id + "\nMsgID: " + message.id + "\n";
     var logPath = `chatLog/${message.guild.id}/${message.channel.id}/chat.log`;
